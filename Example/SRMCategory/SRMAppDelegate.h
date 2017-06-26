33592f635792a673ed42580a2a5d120d3d687360
Mark Song <marksong@augmentum.com.cn>
2017-06-26 16:35:30 +0800

    Initial commit

---
+++ Example/SRMCategory/SRMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SRMAppDelegate.h
+//  SRMCategory
+//
+//  Created by githubsr on 06/26/2017.
+//  Copyright (c) 2017 githubsr. All rights reserved.
+//
+
+@import UIKit;
+
+@interface SRMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
